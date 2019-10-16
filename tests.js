85573c590cd8d0e3b1dd8ccd6cbff0019890e100
MelindaGreene <Melindagreene711@gmail.com>
2019-10-16 09:00:00 -0500

    exercise loops

--- tests.js
+++ tests.js
@@ -13,3 +13,19 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+
+describe("sayHello" , function(){
+    it('should be a defined function' , function() {
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe("string");
+    });
+    // it('should return a string " Hello, Alex!"' , function() {
+    //     expect(sayHello("Alex")).toBe("Hello, Alex!");
+    // });
+    it('should return a string "Hello, Jane!' ,function() {
+        expect(typeof sayHello)).toBe("Hello, Jane!");
+    });
+});
